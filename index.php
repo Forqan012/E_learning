b0a749c44ddb1f91a9aa18d2f4464c7202d51609
Forqan012 <amirdewlpers@gmail.com>
2024-10-05 04:24:45 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Register</title>
+  <!-- Bootstrap CSS link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- Bootstrap Icons link -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, molestiae dolorum. Illo suscipit illum voluptatum veniam ratione? Vitae, nulla a ipsam, autem dolore expedita nostrum ut reprehenderit tempora vero hic? Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sequi illo omnis nisi ad earum qui, ex distinctio veniam exercitationem, maxime suscipit! Nostrum, minima dicta. Quibusdam ullam neque dolore? Velit.</p>
+
+  <!-- Bootstrap JS link -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
